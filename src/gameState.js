5784c085646a16cbebd5d6e54df82606fc21cab3
Laura-deVere <lauradevh@gmail.com>
2020-08-08 15:45:18 -0400

    intial commit

---
+++ src/gameState.js
@@ -0,0 +1,9 @@
+const gameSate = {
+  score: 0,
+  cardFlips: 0,
+  time: 0,
+  level: 1,
+  start() {
+    console.log("started");
+  },
+};
---
+++ src/init.js
@@ -0,0 +1,6 @@
+import game from "./gameState";
+
+const startBTN = document.getElementById("game-start");
+function init() {
+  console.log(start);
+}
